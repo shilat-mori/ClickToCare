ec7f23c11160e34a98578c296b442943115489a4
Sapphire Frommer <atida.student.sf@gmail.com>
2024-12-15 13:39:49 +0200

    changed the way new user card looks

--- src/app/components/newUserCard.tsx
+++ src/app/components/newUserCard.tsx
@@ -4,7 +4,7 @@ import NewUserCardProps from "../types/newUserCardProps"
 import axios from "axios"
 import { UserRole } from "../types/userRole"
 
-const NewUserCard: React.FC<NewUserCardProps> = ({userInfo, aboutMe}) => {
+const NewUserCard: React.FC<NewUserCardProps> = ({ userInfo, aboutMe }) => {
     const verifyUser = async () => {
         const userResponse = await axios.put('/api/users', {
             username: userInfo.username as string,
@@ -13,11 +13,18 @@ const NewUserCard: React.FC<NewUserCardProps> = ({userInfo, aboutMe}) => {
         console.log(userResponse.data);
     };
     return (
-        <div className="w-full flex items-center justify-between p-4 mb-4 rounded-lg border-2 border-sky-500">
-            <div className="font-bold text-lg">{userInfo.username}</div>
-            <div>{userInfo.email}</div>
-            <div>about me: {aboutMe}</div>
-            <button onClick={verifyUser} className="bg-sky-500 text-white p-2 rounded m-2">Verify User</button>
+        <div className="w-full p-4 mb-4 rounded-lg border-2 border-sky-500 flex flex-col gap-4">
+                <div className="font-bold text-lg">{userInfo.username}</div>
+                <div className="text-lg">email: {userInfo.email}</div>
+            <div>About me: </div>
+            <div className="border-2 border-gray-200 w-1/2">{aboutMe}</div>
+            <div className="flex justify-end w-full">
+                <button
+                    onClick={verifyUser}
+                    className="bg-sky-500 text-white px-4 py-2 rounded">
+                    Verify User
+                </button>
+            </div>
         </div>
     )
 }
