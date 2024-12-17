5e32c89254fa9e914f88e73ee67b08085d5d43b6
Sapphire Frommer <atida.student.sf@gmail.com>
2024-12-17 12:33:52 +0200

    added toggle buttons to decide the page renders (time: week/month) & (type: list/graph)

--- src/app/pages/protected/user/myActivity/page.tsx
+++ src/app/pages/protected/user/myActivity/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 import TaskActivityGraph from '@/app/components/TaskActivityGraph';
 import TasksByDay from '@/app/components/taskByDay';
-import React from 'react'
+import React, { useState } from 'react'
 
 const MyActivity = () => {
+  const [type, setType] = useState<"list" | "graph">("list");
+  const [time, setTime] = useState<"week" | "month">("week");
+
   return (
     <div>
-      <TasksByDay range="week" />
-      {/* <TasksByDay range="month" /> */}
-      <TaskActivityGraph view="week" />
+      <div className="flex flex-row">
+        <button className="buttonStyle" onClick={() => setType(type === "list" ? "graph" : "list")}>{type}</button>
+        <button className="buttonStyle" onClick={() => setTime(time === "week" ? "month" : "week")}>{time}</button>
+      </div>
+      {(type === "list") && <TasksByDay range={time} />}
+      {(type === "graph") && <TaskActivityGraph view={time} />}
     </div>
   )
 }
