17479d365cad448a7b1a6b1f9bc4934143439fcf
Shilat Mori <7684129@gmail.com>
2025-01-05 00:37:08 +0200

    styling the rejected user page

--- src/app/components/waitingComponents/countdownBlock.tsx
+++ src/app/components/waitingComponents/countdownBlock.tsx
@@ -1,10 +1,10 @@
 import CountdownBlockProps from '@/app/types/waiting/countdownBlockProps'
 import React from 'react'
  
-const CountdownBlock: React.FC<CountdownBlockProps> = ({ name, num }) => {
+const CountdownBlock: React.FC<CountdownBlockProps> = ({ name, num, lighting }) => {
     return (
         <div className="flex flex-col items-center">
-            <span className="waiting-message text-6xl">{num}</span>
+            <span className={`${lighting?'waiting-message':''} text-6xl`}>{num}</span>
             <span className="text-sm uppercase tracking-wide">{name}</span>
         </div>
     )
